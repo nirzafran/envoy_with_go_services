671098b0d8c06bc0bc61e7d015ddadc66a031711
Nir Moav <nir@zafran.io>
2023-05-09 23:05:04 +0300

    creating the gRPC GO server

--- apps/go.mod
+++
@@ -1,3 +0,0 @@
-module getnirm/goserver
-
-go 1.20
--- apps/server/server.go
+++ apps/server/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
-	"strconv"
 	"time"
+
+	pb "getnirm/goserver/generated"
+
+	"google.golang.org/grpc"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,12 +20,35 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world " + currentTime))
 }
 
+const (
+	listenAddress = "0.0.0.0:8090"
+)
+
+type timeService struct {
+}
+
+func (t *timeService) GetCurrentTime(ctx context.Context, req *pb.GetCurrentTimeRequest) (*pb.GetCurrentTimeResponse, error) {
+	log.Println("Got time request")
+	return &pb.GetCurrentTimeResponse{CurrentTime: time.Now().String()}, nil
+}
+
 func main() {
-	const port = 8080
-	fmt.Printf("Starting server on port %d", port)
-	http.HandleFunc("/", rootHandler)
-	// convert port to string
-	// start the server on port 8080
-	serverPort := ":" + strconv.Itoa(port)
-	http.ListenAndServe(serverPort, nil)
+	// const port = 8080
+	fmt.Printf("Starting server on port %s", listenAddress)
+	// http.HandleFunc("/", rootHandler)
+	// // convert port to string
+	// // start the server on port 8080
+	// serverPort := ":" + strconv.Itoa(port)
+	// http.ListenAndServe(serverPort, nil)
+
+	lis, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterTimeServiceServer(s, &timeService{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
